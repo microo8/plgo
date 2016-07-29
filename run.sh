1749baf474ef7021bfe9ec4d38fa673bcf3a130e
Mgr. Vladimir Magyar <magyarvladimir@gmail.com>
2016-07-29 09:42:01 +0200

    MIT

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-go build -v -buildmode=c-shared -o example.so example.go pl.go && sudo cp example.so /usr/lib/postgresql &&
+go build -v -x -buildmode=c-shared -o example.so example.go pl.go && sudo cp example.so /usr/lib/postgresql &&
 sudo systemctl restart postgresql.service && psql -U root -d meh -c "select plgo_example('foo', 10)"
 rm example.so
