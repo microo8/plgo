fad879856da913eadb79ec0266b7b1b21933d7d6
Mgr. Vladimir Magyar <magyarvladimir@gmail.com>
2018-06-24 13:29:29 +0200

    bgw experiments (cannot run goroutine)

--- test/bgw/bdw.go
+++ test/bgw/bdw.go
@@ -19,28 +19,30 @@ package main
 PG_MODULE_MAGIC;
 
 void _PG_init(void);
-extern int BackgroundWorkerMain();
+extern int backgroundWorkerMain();
 
 void elog_log(char *string) {
 	elog(LOG, string, "");
 }
 
 int wait_latch(long miliseconds) {
-	char text[1000];
-	sprintf(text, "start waiting for max %ld miliseconds", miliseconds);
-	elog_log(text);
 #if (PG_VERSION_NUM >= 100000)
 		return WaitLatch(MyLatch, waitFlags, miliseconds, PG_WAIT_EXTENSION) & WL_POSTMASTER_DEATH;
 #else
-		return WaitLatch(MyLatch, waitFlags, miliseconds) & WL_POSTMASTER_DEATH;
+		return WaitLatch(&MyProc->procLatch, waitFlags, miliseconds) & WL_POSTMASTER_DEATH;
 #endif
 }
 
 void reset_latch(void) {
+#if (PG_VERSION_NUM >= 100000)
 	ResetLatch(MyLatch);
+#else
+	ResetLatch(&MyProc->procLatch);
+#endif
 }
 
 static volatile sig_atomic_t got_sigterm = false;
+static volatile sig_atomic_t got_sighup = false;
 
 int get_got_sigterm() {
 	return (got_sigterm == true);
@@ -52,59 +54,52 @@ static void background_sigterm(SIGNAL_ARGS)
 {
 	int save_errno = errno;
 	got_sigterm = true;
-	if (MyProc != NULL) {
-		SetLatch(&MyProc->procLatch);
-	}
+#if (PG_VERSION_NUM >= 100000)
+	SetLatch(MyLatch);
+#else
+	if (MyProc != NULL)	SetLatch(&MyProc->procLatch);
+#endif
 	errno = save_errno;
 }
 
 static void background_sighup(SIGNAL_ARGS) {
-	//CronJobCacheValid = false;
-
-	if (MyProc != NULL)
-	{
+	int	save_errno = errno;
+	got_sighup = true;
+#if PG_VERSION_NUM >= 100000
+	SetLatch(MyLatch);
+#else
+	if (MyProc)
 		SetLatch(&MyProc->procLatch);
-	}
+#endif
+	errno = save_errno;
 }
 
 void background_main(Datum main_arg) {
 	pqsignal(SIGHUP, background_sighup);
-	pqsignal(SIGINT, SIG_IGN);
+	//pqsignal(SIGINT, SIG_IGN);
 	pqsignal(SIGTERM, background_sigterm);
 	BackgroundWorkerUnblockSignals();
-
-	int rc;
-
-	//if (BackgroundWorkerMain()) {
-	while (!got_sigterm) {
-		rc = WaitLatch(MyLatch, waitFlags, 1000L);
-		ResetLatch(MyLatch);
-
-		if (rc & WL_POSTMASTER_DEATH) {
-			elog_log("bgw exited, postmaster is dead");
-			proc_exit(1);
-		}
-		reset_latch();
-		elog_log("Hello from C!!!!!!!!!!!!!");
-	}
-
-	elog_log("quit BackgroundWorkerMain");
-	proc_exit(0);
+	proc_exit(backgroundWorkerMain());
 }
 
 
 void _PG_init(void) {
 	BackgroundWorker worker;
-
 	MemSet(&worker, 0, sizeof(BackgroundWorker));
 	worker.bgw_flags = BGWORKER_SHMEM_ACCESS;
 	worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
-	snprintf(worker.bgw_library_name, BGW_MAXLEN, "bgw");
-	snprintf(worker.bgw_function_name, BGW_MAXLEN, "background_main");
 	snprintf(worker.bgw_name, BGW_MAXLEN, "GoBackgroundWorker");
+#if PG_VERSION_NUM >= 100000
+	sprintf(worker.bgw_library_name, "bgw");
+	sprintf(worker.bgw_function_name, "backgroundWorkerMain");
+#else
+	worker.bgw_main = bg_mon_main;
+#endif
 	worker.bgw_restart_time = BGW_NEVER_RESTART;
 	worker.bgw_main_arg = (Datum) 0;
+#if PG_VERSION_NUM >= 90400
 	worker.bgw_notify_pid = 0;
+#endif
 	RegisterBackgroundWorker(&worker);
 }
 */
@@ -121,11 +116,36 @@ func Log(text string) {
 	C.elog_log(C.CString(text))
 }
 
-//WaitLatch will wait the duration, but can be interrupted, so use this and not time.Sleep
+//WaitLatch ...
 func WaitLatch(d time.Duration) error {
 	if rc := C.wait_latch(C.long(d.Nanoseconds() / 1000000)); rc != 0 {
 		return errors.New("postmaster is dead")
 	}
 	C.reset_latch()
 	return nil
+}
+
+//Tick ...
+func Tick(d time.Duration) <-chan bool {
+	ch := make(chan bool)
+	Log("0")
+	go func() {
+		Log("1")
+		for C.get_got_sigterm() == 0 {
+			Log("2")
+			if rc := C.wait_latch(C.long(d.Nanoseconds() / 1000000)); rc != 0 {
+				Log("3")
+				ch <- false
+				return
+			}
+			Log("4")
+			C.reset_latch()
+			Log("5")
+			ch <- true
+			Log("6")
+		}
+		Log("7")
+	}()
+	Log("8")
+	return ch
 }
