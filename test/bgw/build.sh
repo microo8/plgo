bcedc23faa459c3aa627c2790d0b7313f23aae6a
Vladimir Magyar <magyarvladimir@gmail.com>
2020-04-23 18:47:46 +0200

    moved to gitlab

--- test/bgw/build.sh
+++
@@ -1,1 +0,0 @@
-go build -buildmode=c-shared -o bgw.so && sudo cp bgw.so `pg_config --pkglibdir` && sudo chmod 755 `pg_config --pkglibdir`/* && sudo systemctl restart postgresql.service
--- test/plgo_test.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE FUNCTION public.plgo_test()
-  RETURNS void AS
-'$libdir/plgo_test', 'PLGoTest'
-  LANGUAGE c IMMUTABLE STRICT;
